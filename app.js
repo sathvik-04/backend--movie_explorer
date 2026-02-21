1d3bb4994b2980a03a988c9041ced61557096685
sathvik shetty <sathvikshetty0404@gmail.com>
2026-02-21 11:50:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("Hello World!");
+app.get("/", (req, res) => {
+  res.json({
+    message: "Movie Explorer Backend API is running 🚀",
+    endpoints: {
+      auth: "/api/auth",
+      watchlist: "/api/watchlist"
+    }
+  });
+});
